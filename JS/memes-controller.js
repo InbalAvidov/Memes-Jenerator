aac03d4844454807fbf001eb915bbedce8e0ca3f
Inbal Avidov <inbal.avidov@gmail.com>
2022-12-17 19:40:31 +0200

    done!

--- JS/memes-controller.js
+++ JS/memes-controller.js
@@ -1,9 +1,9 @@
 'use strict'
 function renderMemes() {
-    document.querySelector(".search").style.display = 'none'
     document.body.classList.remove("open-menu")
     const memes = loadFromStorage(STORAGE_KEY)
     if (memes.length === 0) return
+    document.querySelector(".search").style.display = 'none'
     var srtHTML = memes.map((meme) => `<img id="${meme.meme.selectedImgId}" class="meme-img" onclick="onEditSaved(this)" src="${meme.url}">`)
     document.querySelector('.imgs-container').innerHTML = srtHTML.join('')
 }
